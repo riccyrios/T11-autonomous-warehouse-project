95cce01af3539ca4eafe9f8fdf24e4d19cd0d601
Yara Fakoua <yara.fakoua@student.uts.edu.au>
2024-05-07 12:23:02 +1000

    main workspace

---
+++ main_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/git/T11_multi_warehouse/main_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
