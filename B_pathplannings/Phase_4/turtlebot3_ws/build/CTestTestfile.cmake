d1721bf313cc87ba080a2e3b3996b8d7330658b5
Bo <yongqi.su@student.uts.edu.au>
2024-05-10 15:48:47 +1000

    second commit!

--- B_pathplannings/Phase_4/turtlebot3_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bananabreadbo/git/path-planning-astar-turtlebot/Phase_4/turtlebot3_ws/src
-# Build directory: /home/bananabreadbo/git/path-planning-astar-turtlebot/Phase_4/turtlebot3_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("turtlebot3_astar")
--- B_pathplannings/Phase_4/turtlebot3_ws/build/turtlebot3_astar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bananabreadbo/git/path-planning-astar-turtlebot/Phase_4/turtlebot3_ws/src/turtlebot3_astar
-# Build directory: /home/bananabreadbo/git/path-planning-astar-turtlebot/Phase_4/turtlebot3_ws/build/turtlebot3_astar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
