3ff4924ca29adc906ffdfeb94156c651c32ad883
riccrios <ricardo.rios@student.edu.au>
2024-05-28 18:41:59 +1000

    re-updating R_controls

--- R_controls/turtlebot_project/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/riccrios/catkin_ws/src/turtlebot_project
-# Build directory: /home/riccrios/catkin_ws/src/turtlebot_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
