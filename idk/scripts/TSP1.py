c6630cf2fe540b7838dd7a814621a7a5890464b1
ckimjudson <160566238+ckimjudson@users.noreply.github.com>
2024-05-28 10:29:33 +1000

    TSP THis one !!!
    Guys please download the LKH solver to your Downloads if you don't want to change the file paths in the code. Otherwise, at the top of the TSP1 and TSP2 files change the file paths to your $USER and such and such

--- idk/scripts/TSP1.py
+++ idk/scripts/TSP1.py
@@ -14,17 +14,25 @@ import os
 
 NODE_NAME = "taskGeneration"
 
-#*** File Paths for writing ***#
+#*** File Paths for reading/writing ***#
 """Change to your own filepaths if you run on your own machine"""
+base_path = os.path.join(os.path.expanduser("~"), "Downloads/LKH-3.0.9")
 
-CONVERSION_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_CONVERSION.txt"
-COORDINATE_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_COORDINATE.txt"
-PAR_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_TEST.par"
-TASK_GENERATION_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_TEST.txt"
-BOS_EDGE_WEIGHTS_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/Bos_Path_Planning_Output.txt"
+#CONVERSION_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_CONVERSION.txt"
+#COORDINATE_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_COORDINATE.txt"
+#PAR_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_TEST.par"
+#TASK_GENERATION_FILE_PATH = "/home/chloe/Downloads/LKH-3.0.9/TSP_TEST.txt"
+#BO = "/home/chloe/Downloads/LKH-3.0.9/distance_matrix.txt"
+#NEW_MATRIX = "/home/chloe/Downloads/LKH-3.0.9/matrixMaker.txt"
+
+CONVERSION_FILE_PATH = os.path.join(base_path, "TSP_CONVERSION.txt")
+COORDINATE_FILE_PATH = os.path.join(base_path, "TSP_COORDINATE.txt")
+PAR_FILE_PATH = os.path.join(base_path, "TSP_TEST.par")
+TASK_GENERATION_FILE_PATH = os.path.join(base_path, "TSP_TEST.txt")
+BO = os.path.join(base_path, "distance_matrix.txt")
+NEW_MATRIX = os.path.join(base_path, "matrixMaker.txt")
 
 #*** Make a global dictionary for other nodes to access ***#
-# This is not needed if path planning costs are used and there is no graphical representation
 
 NODE_COORDINATES = {
     0: (0.00, 0.00),
@@ -50,10 +58,89 @@ NODE_COORDINATES = {
     20: (0.84, 1.34)
 }
 
+# This is the full distance matrix for all nodes. this is not dynamic. Change to subscribe/reading txt file
+FULL_DISTANCE_MATRIX = [
+    [0, 7.36, 7.79, 5.35, 5.02, 4.42, 4.52, 3.93, 3.83, 4.62, 4.95, 6.7, 7.09, 2.44, 2.24, 4.39, 4.79, 6.93, 7.19, 1.78, 1.85],
+    [7.36, 0, 0.2, 2.28, 2.61, 4.79, 4.98, 5.02, 4.52, 2.9, 2.54, 0.5, 0.5, 5.38, 4.98, 3.23, 2.97, 2.28, 2.21, 5.87, 5.51],
+    [7.79, 0.2, 0, 2.67, 2.77, 5.02, 5.25, 5.11, 4.52, 2.8, 2.11, 0.4, 0.59, 4.98, 4.59, 3.2, 2.8, 2.18, 2.44, 5.44, 5.08],
+    [5.35, 2.28, 2.67, 0, 0.2, 2.24, 2.64, 2.84, 2.94, 0.66, 0.59, 2.24, 2.64, 3.4, 3.3, 2.28, 2.18, 3.07, 2.97, 3.89, 3.53],
+    [5.02, 2.61, 2.77, 0.2, 0, 3.2, 3.37, 2.97, 2.14, 0.4, 0.82, 3.56, 3.4, 3.1, 2.87, 2.24, 2.67, 3.37, 3.76, 3.7, 3.3],
+    [4.42, 4.79, 5.02, 2.24, 3.2, 0, 0.2, 0.5, 0.59, 2.31, 3.23, 4.98, 5.08, 2.31, 2.31, 3.56, 4.49, 5.18, 5.15, 2.9, 2.9],
+    [4.52, 4.98, 5.25, 2.64, 3.37, 0.2, 0, 0.59, 0.79, 2.67, 3.3, 4.82, 5.21, 2.15, 2.31, 3.13, 3.53, 5.31, 5.71, 2.54, 3.0],
+    [3.93, 5.02, 5.11, 2.84, 2.97, 0.5, 0.59, 0, 0.69, 2.84, 3.3, 5.05, 5.61, 1.78, 1.95, 3.33, 3.5, 5.34, 5.91, 2.34, 2.54],
+    [3.83, 4.52, 4.52, 2.94, 2.14, 0.59, 0.79, 0.69, 0, 2.71, 2.67, 4.42, 4.88, 1.98, 1.72, 2.97, 3.33, 5.64, 5.12, 2.21, 2.74],
+    [4.62, 2.9, 2.8, 0.66, 0.4, 2.31, 2.67, 2.84, 2.71, 0, 0.89, 3.1, 3.23, 2.64, 2.24, 1.75, 1.95, 3.1, 3.17, 2.84, 2.74],
+    [4.95, 2.54, 2.11, 0.59, 0.82, 3.23, 3.3, 3.3, 2.67, 0.89, 0, 2.05, 2.44, 3.07, 2.87, 1.72, 1.91, 2.94, 3.14, 3.47, 3.27],
+    [6.7, 0.5, 0.4, 2.24, 3.56, 4.98, 4.82, 5.05, 4.42, 3.1, 2.05, 0, 1.15, 4.62, 4.49, 2.71, 2.31, 1.62, 2.08, 5.12, 4.88],
+    [7.09, 0.5, 0.59, 2.64, 3.4, 5.08, 5.21, 5.61, 4.88, 3.23, 2.44, 1.15, 0, 5.68, 4.82, 3.07, 2.8, 1.68, 1.52, 5.41, 5.11],
+    [2.44, 5.38, 4.98, 3.4, 3.1, 2.31, 2.15, 1.78, 1.98, 2.64, 3.07, 4.62, 5.68, 0, 0.5, 2.97, 3.37, 5.51, 5.71, 0.56, 0.79],
+    [2.24, 4.98, 4.59, 3.3, 2.87, 2.31, 2.31, 1.95, 1.72, 2.24, 2.87, 4.49, 4.82, 0.5, 0, 3.23, 3.47, 4.59, 5.12, 0.5, 0.59],
+    [4.39, 3.23, 3.2, 2.28, 2.24, 3.56, 3.13, 3.33, 2.97, 1.75, 1.72, 2.71, 3.07, 2.97, 3.23, 0, 0.66, 2.74, 3.33, 3.76, 3.6],
+    [4.79, 2.97, 2.8, 2.18, 2.67, 4.49, 3.53, 3.5, 3.33, 1.95, 1.91, 2.31, 2.8, 3.37, 3.47, 0.66, 0, 2.21, 2.77, 3.3, 3.0],
+    [6.93, 2.28, 2.18, 3.07, 3.37, 5.18, 5.31, 5.34, 5.64, 3.1, 2.94, 1.62, 1.68, 5.51, 4.59, 2.74, 2.21, 0, 0.79, 5.35, 5.41],
+    [7.19, 2.21, 2.44, 2.97, 3.76, 5.15, 5.71, 5.91, 5.12, 3.17, 3.14, 2.08, 1.52, 5.71, 5.12, 3.33, 2.77, 0.79, 0, 6.3, 6.17],
+    [1.78, 5.87, 5.44, 3.89, 3.7, 2.9, 2.54, 2.34, 2.21, 2.84, 3.47, 5.12, 5.41, 0.56, 0.5, 3.76, 3.3, 5.35, 6.3, 0, 0.66],
+    [1.85, 5.51, 5.08, 3.53, 3.3, 2.9, 3.0, 2.54, 2.74, 2.74, 3.27, 4.88, 5.11, 0.79, 0.59, 3.6, 3.0, 5.41, 6.17, 0.66, 0]
+]
+
 original_to_reference_mapping = {}
 selected_nodes = []
 num_nodes_to_pick = 0
 
+#*** Matrix Making Functions and Process ***#
+# The LKH solver needs a distance matrix of the nodes to provide a distance based cost solution
+# This should be set up as a rosservice to request the full node distance matrix
+# In lieu, this code readds from a text file.
+""" 1. Finding the real node number from the reference number
+
+    2. Creating a smaller distance matrix that only includes the selected_nodes
+    2.1 Iterate over each pair of nodes in selected_nodes
+    2.2 Retrieve the real node numbers from the reference numbers
+    2.3 Retrieve the corresponding distance from the full distance matrix
+    2.4 Add the distance to the row until all distances are added
+
+    3. Write the smaller distance matrix to a file for the TSP solver to read
+    3.1 Open the file and write each row of the matrix to the file as strings
+    3.2 Separate each distance value with a tab (I hope this doesnt mess up reading it)
+"""
+
+def get_real_node(reference_number):
+    return original_to_reference_mapping[reference_number]
+
+def create_smaller_distance_matrix(FULL_DISTANCE_MATRIX, selected_nodes):
+    smaller_distance_matrix = []
+
+    for node1, _, _ in selected_nodes:
+        row = []
+        real_node1 = get_real_node(node1)
+        for node2, _, _ in selected_nodes:
+            real_node2 = get_real_node(node2)
+            # Retrieve distance from full matrix based on real node numbers
+            row.append(FULL_DISTANCE_MATRIX[real_node1][real_node2])
+        smaller_distance_matrix.append(row)
+
+    return smaller_distance_matrix
+
+def write_distance_matrix_to_file(new_distance_matrix, NEW_MATRIX):
+    with open(NEW_MATRIX, 'w') as file:
+        for row in new_distance_matrix:
+            # Convert each distance value to a string and join them with tabs
+            row_str = '\t'.join(map(str, row))
+            # Write the row to the file
+            file.write(row_str + '\n')
+
+#*** Random Node Picking process for the parameter file ***#
+            
+# The LKH solver requires the parameter file to label the nodes with reference numbers starting from 1 and incrementing by 1.      
+## This has been designed so that a user input number of node locations can be chosen, and the code will randomly select that number from the pool of locations.
+""" 1. Choose length of pick list
+    2. Create pick list and keep track of reference and original node numbers
+    3. Create the necessary files for the LKH Solver:
+        * Parameter file
+        * Problem file
+        * Reference node to coordinate mapping file
+        * Reference node to original node mapping file
+"""
 def generate_tasks(num_nodes_to_pick):
     global selected_nodes
     selected_nodes = []
@@ -80,8 +167,11 @@ def generate_tasks(num_nodes_to_pick):
 
 def task_generation(num_nodes_to_pick):
     global original_to_reference_mapping
-    rospy.init_node('taskGeneration', anonymous=True)
+    #rospy.init_node('taskGeneration', anonymous=True)
     selected_nodes, original_to_reference_mapping = generate_tasks(num_nodes_to_pick)
+
+    smaller_distance_matrix = create_smaller_distance_matrix(FULL_DISTANCE_MATRIX, selected_nodes)
+    write_distance_matrix_to_file(smaller_distance_matrix, NEW_MATRIX)
 
     with open(PAR_FILE_PATH, 'w') as file:
         file.write("PROBLEM_FILE = /home/chloe/Downloads/LKH-3.0.9/TSP_TEST.txt\n")
@@ -90,6 +180,8 @@ def task_generation(num_nodes_to_pick):
 
     with open(CONVERSION_FILE_PATH, 'w') as file:
         file.write("{\n")
+        global reference
+        global original
         for reference, original, (x, y) in selected_nodes:
             file.write('    "{}": {},\n'.format(reference, original))
         file.seek(0, os.SEEK_END)
@@ -106,6 +198,7 @@ def task_generation(num_nodes_to_pick):
         file.truncate()
         file.write("\n}")
 
+    """
     with open(TASK_GENERATION_FILE_PATH, 'w') as file:
         file.write("NAME : TSP_TEST\n") 
         file.write("TYPE : TSP\n")
@@ -119,6 +212,29 @@ def task_generation(num_nodes_to_pick):
         for reference, original_node, (x, y) in selected_nodes:
             file.write("{} {} {}\n".format(reference, x, y))
 
+        file.write("EOF\n")
+    """
+
+    with open(NEW_MATRIX, 'r') as file:
+        path_planning_edge_weights = file.read()
+    
+    with open(TASK_GENERATION_FILE_PATH, 'w') as file:
+        file.write("NAME : TSP_TEST\n") 
+        file.write("TYPE : TSP\n")
+        file.write("COMMENT : Chloe Judson, Bo Coordinate Test\n")
+        #file.write("DIMENSION : {}\n".format(num_nodes_to_pick + 1))
+        file.write("DIMENSION : {}\n".format(num_nodes_to_pick + 1))
+        #*** Change EGDE_WEIGHT_TYPE to EXPLICIT ***#
+        file.write("EDGE_WEIGHT_TYPE : EXPLICIT\n")
+        #*** Add EDGE_WEIGHT_FORMAT to LOWER_DIAG_ROW/LOWER_ROW/FULL_MATRIX ***#
+        file.write("EDGE_WEIGHT_FORMAT : FULL_MATRIX\n")
+        #*** Change DISPLAY_DATA_TYPE to TWOD_DISPLAY ***#
+        file.write("DISPLAY_DATA_TYPE : TWOD_DISPLAY\n")
+        #*** Add Node Coordinates to the file ***#
+        #*** Change NODE_COORD_SECTION to EDGE_WEIGHT_SECTION ***#
+        file.write("EDGE_WEIGHT_SECTION\n")
+        #*** Write the edge weights from Bo's Path Planning to the task generation file ***#
+        file.write(path_planning_edge_weights)
         file.write("EOF\n")
 
 def node_picking_num():
@@ -138,4 +254,6 @@ def main():
     pass
   
 if __name__ == '__main__':
+    rospy.init_node('taskGeneration', anonymous=True)
+    # INITIALISE NODE HERE!!
     main()
--- idk/scripts/TSPMain.py
+++ idk/scripts/TSPMain.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import rospy
+
 from TSP1 import main as TSP1_main
 from TSP2 import main as TSP2_main
 
@@ -8,5 +10,5 @@ def main():
     TSP2_main()
 
 if __name__ == '__main__':
-    
+    rospy.init_node('NODE_NAME', anonymous=True)
     main()
