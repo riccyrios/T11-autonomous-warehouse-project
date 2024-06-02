b04e52cd6cbcc89f6dd04be01e8016f6d6bc53cd
riccrios <ricardo.rios@student.edu.au>
2024-06-02 20:35:10 +1000

    new controls code -

--- turtlebot_project/src/turtlebot_control.cpp
+++ turtlebot_project/src/turtlebot_control.cpp
@@ -2,346 +2,181 @@
 #include "tf/transform_datatypes.h"
 #include <cmath>
 
-TurtlebotControl::TurtlebotControl(ros::NodeHandle nh, int ID): nh_(nh), turtlebot_ID_(ID){
+TurtlebotControl::TurtlebotControl(ros::NodeHandle nh, int ID): 
+    nh_(nh), 
+    turtlebot_ID_(ID), 
+    current_waypoint_index_(0), 
+    lin_vel_(0),
+    ang_vel_(0),
+    linear_tolerance_(0.1),
+    angular_tolerance_(25*M_PI/180) //adjust
+{
 
-    std::cout << "TurtlebotControl constructor" << std::endl;
-
-    // Initialize the subscriber and publisher
-    //odom_info_ = nh_.subscribe("/odom", 1, &TurtlebotControl::odoCallback, this);
     odom_info_ = nh_.subscribe("/odom", 1, &TurtlebotControl::odoCallback, this);
-    //cmd_vel_pub = nh_.advertise<geometry_msgs::Twist>("/tb"+std::to_string(turtlebot_ID_)+"/cmd_vel", 1);
+
     cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+
+    waypointSet_ = std::vector<std::vector<geometry_msgs::Point>>();
+
+    std::string file_path = "/home/riccrios/catkin_ws/src/turtlebot_project/src/paths.txt"; /////CHANGEE THISS
+
+    loadWaypoints_(file_path);
 
     // potentially add the waypoint publisher, we should subscribe to a ros topic that subscribes to the waypoints being published by path planning node
     //waypoints_sub_ = nh_.subscribe("/waypoints", 1, &TurtlebotControl::setWaypoints, this);
 
-    linear_tolerance_ = 0.1;
-    angular_tolerance_ = 5*M_PI/180; //might need to make this tolerance smaller too wide right now i think
 }
 
-//void TurtlebotControl::setWaypoints(const std::vector<geometry_msgs::Point>::ConstPtr& waypoints){
-    //waypoints_ = *waypoints;
-void TurtlebotControl::setWaypoints(std::vector<geometry_msgs::Point> waypoints){
+void TurtlebotControl::loadWaypoints_(std::string& file_path){
 
-    std::cout << "setwaypoints function started for turtlebot" << std::endl;
+    std::ifstream infile(file_path);
 
-    // this is where i'll accept the points from Bo, subscribe to the topic that publishes the waypoints
+    if (!infile.is_open()) {
+        ROS_ERROR("Failed to open paths.txt");
+        return;
+    }
 
-    // Set the waypoints
-    waypoints_ = waypoints;
+    std::string line;
 
-    // Set the current waypoint to the first waypoint
-    current_point_index_ = 0;
-    currentWayPoint_ = waypoints_[current_point_index_];
+    while(std::getline(infile, line)){
 
+        std::istringstream iss(line);
+        std::vector<geometry_msgs::Point> waypoints;
+        double x, y;
+        char bracket1, comma, bracket2;
 
+        while (iss >> bracket1 >> x >> comma >> y >> bracket2) {
+            geometry_msgs::Point point;
+            point.x = x;
+            point.y = y;
+            waypoints.push_back(point);
+        }
 
-    // Set the current goal to the last waypoint
-    currentGoal_ = waypoints_[waypoints_.size()-1];
-
+        waypointSet_.push_back(waypoints);
+    }
 }
 
 void TurtlebotControl::moveTurtlebot(){
 
-    //////////////////////////////////////////
-    // while (ros::ok()) {
-    //     if (waypoints_.empty()) {
-    //         ROS_WARN("No valid points available. Exiting...");
-    //         return;
-    //     }
-
-    //     // Calculate direction to the next point
-    //     ROS_INFO_STREAM("Current waypoint: " << currentWayPoint_.x << ", " << currentWayPoint_.y);
-    //     ROS_INFO_STREAM("Odom position: " << odo_.pose.pose.position.x << ", " << odo_.pose.pose.position.y);
-
-    //     double dx = currentWayPoint_.x- odo_.pose.pose.position.x;
-    //     double dy = currentWayPoint_.y - odo_.pose.pose.position.y;
-    //     double distance_to_target = sqrt(dx * dx + dy * dy);
-
-    //     // Print the number of points left to reach
-    //     int points_left = waypoints_.size() - current_point_index_;
-    //     //ROS_INFO_STREAM("Points left to reach: " << points_left);
-
-    //     // Calculate angle to the next point
-    //     double angle_to_target = atan2(dy, dx);
-
-    //     double angle_to_target_degrees = angle_to_target * 180 / M_PI;
-
-    //     //Initalise vleocities 
-    //     double linear_vel = 0;
-    //     double angular_vel = 0;
-    //     double bot_angle = tf::getYaw(odo_.pose.pose.orientation);
-    //     double calculate_angular_vel = 0.5 * (angle_to_target - bot_angle);
-
-    
-    //     angular_vel = calculate_angular_vel;   
-    //     // Set maximum linear velocity
-    //     double max_linear_vel = 0.3;
-    //     // Set minimum linear velocity
-    //     double min_linear_vel = 0.1;
-
-    //     // // Set angle tolerance
-    //     // double angle_tolerance = 20; //in degress Use angle_to_target_degrees when refering to this 
-    //     // double angle_threshold = M_PI / 4; //in degrees Use angle_to_target_degrees when refering to this
-
-    //     //Speed increment
-    //     double speedIn = 0.01;
-
-    //     // Calculate angle difference between current orientation and angle to the target
-    //     double angle_difference = fabs(angle_to_target - bot_angle);
-
-    //     // Define a threshold angle difference for considering the turtlebot facing the target
-    //     double facing_threshold = 20 * M_PI / 180; // 20 degrees in radians
-
-    //     // Check if the angle difference is within the threshold
-    //     if (angle_difference < facing_threshold && !points_left <= 2)
-    //     {
-    //         // Turtlebot is facing the target, so you can perform some action here
-    //         ROS_INFO("Turtlebot is facing the target!");
-    //         linear_vel = linear_vel + speedIn; //speed up since turtlebot is on track
-    //         ROS_INFO_STREAM("Linear FT Velocity: " << linear_vel); //FT Facing Target
-
-    //         angular_vel = 0; // no need for rotatiion, turtlebot is on track
-
-    //         // Ensure linear velocity does not exceed maximum or fall below minimum
-    //     if (linear_vel > max_linear_vel) {
-    //         linear_vel = max_linear_vel;
-    //     }
-        
-    //     if (linear_vel < min_linear_vel && linear_vel != 0) {
-    //         linear_vel = min_linear_vel;
-    //     }
-
-
-    //     }
-    //     else
-    //     {
-    //         // Turtlebot is not facing the target yet
-    //         ROS_INFO("Turtlebot is not facing the target yet...");
-    //         linear_vel = linear_vel - speedIn; // slow down as turtlebot needs more time to roate
-    //         ROS_INFO_STREAM("Linear NFT Velocity: " << linear_vel); //NFT Not Facing Target
-
-    //         angular_vel = calculate_angular_vel; // rotate the turtlebot so its within the cone of tolerance
-    //         ROS_INFO_STREAM("Angular NFT Velocity: " << angular_vel);
-    //         // Ensure linear velocity does not exceed maximum or fall below minimum
-    //     if (linear_vel > max_linear_vel) {
-    //         linear_vel = max_linear_vel;
-    //     }
-        
-    //     if (linear_vel < min_linear_vel && linear_vel != 0) {
-    //         linear_vel = min_linear_vel;
-    //     }
-
-    //     }
-
-    //     // If the current point is the final point or there are 2 points left, reduce linear velocity
-    //     if (points_left <= 2) {
-    //         linear_vel = linear_vel- speedIn; // Adjust as needed
-
-    //         // Ensure linear velocity does not exceed maximum or fall below minimum
-    //     if (linear_vel > max_linear_vel) {
-    //         linear_vel = max_linear_vel;
-    //     }
-        
-    //     if (linear_vel < min_linear_vel && linear_vel != 0) {
-    //         linear_vel = min_linear_vel;
-    //     }
-
-    //     }
-
-    //     // // Ensure linear velocity does not exceed maximum or fall below minimum
-    //     // if (linear_vel > max_linear_vel) {
-    //     //     linear_vel = max_linear_vel;
-    //     // }
-        
-    //     // if (linear_vel < min_linear_vel && linear_vel != 0) {
-    //     //     linear_vel = min_linear_vel;
-    //     // }
-
-    //     if (distance_to_target < 0.1) {
-    //         // Reached the target point
-    //         ROS_INFO_STREAM("Reached point " << current_point_index_+1);
-
-    //         // Move to the next point
-    //         current_point_index_++;
-    //         if (current_point_index_ >= waypoints_.size()){
-    //             // Reached the final point, stop the Turtlebot
-    //             linear_vel = 0;
-    //             angular_vel = 0;
-    //             stopTurtlebot();
-    //             return;
-    //         }
-
-    //         currentWayPoint_ = waypoints_[current_point_index_];
-    //     }
-
-    //     // Publish the velocity commands
-    //     geometry_msgs::Twist vel_cmd;
-    //     vel_cmd.linear.x = linear_vel;
-    //     vel_cmd.angular.z = angular_vel;
-
-    //     //ROS_INFO_STREAM("Linear Velocity: " << linear_vel);
-    //     //ROS_INFO_STREAM("Angle to Target Degrees: " << angle_to_target_degrees);
-
-    //     cmd_vel_pub_.publish(vel_cmd);
-
-    //     ros::spinOnce();
-    //     rate_.sleep();
-    // }
-
-// ///////////////////////////////////////////////
-
-
-    if (waypoints_.size() == 0){
+    if (waypointSet_.size() == 0){
         ROS_WARN("No waypoints set for turtlebot. Exiting...");
         return;
     }
 
     ros::Rate wait_rate(1);
+
     while(ros::ok() && !odom_received_){
         ROS_INFO("Waiting for odometry information...");
         ros::spinOnce();
         wait_rate.sleep();
     }
 
-    double lin_vel = 0; // Linear velocity
-    double ang_vel = 0; // Angular velocity
+    double waypointSet_index = 0; // Index of the current waypoint Set
 
-    double waypoint_index = 0; // Index of the current waypoint
+    while (waypointSet_index < waypointSet_.size()){
 
-    std::cout << "initial odometry: " << odo_.pose.pose.position.x << " " << odo_.pose.pose.position.y << std::endl;
+        std::cout << "waypointSet index: " << waypointSet_index << std::endl;
+        std::cout << "waypoint set size: " << waypointSet_.size() << std::endl;
 
-    while (waypoint_index < waypoints_.size()){
+        linear_tolerance_ = 0.5;
+        angular_tolerance_ = 30*M_PI/180;
 
-        std::cout << "waypoint index: " << waypoint_index << std::endl;
-        std::cout << "waypoint size: " << waypoints_.size() << std::endl;
 
-        double dx = waypoints_[waypoint_index].x - odo_.pose.pose.position.x;
-        double dy = waypoints_[waypoint_index].y - odo_.pose.pose.position.y;
+        double waypoints_index = 0; // Index of the current waypoints
+        while (waypoints_index < waypointSet_[waypointSet_index].size()){
 
-        double distance_to_point = std::hypot(dx, dy);
+            std::vector<geometry_msgs::Point> waypoints = waypointSet_[waypointSet_index];
 
-        std::cout << "distance to point: " << distance_to_point << std::endl;
+            if (waypoints_index == (waypoints.size()*0.9)){
+                ROS_INFO("Reaching close to final goal");
+                linear_tolerance_ = 0.1;
+                angular_tolerance_ = 10*M_PI/180;
+               
+            }
 
-        std::cout << "about to get angle of rotation" << std::endl;
-        double bot_rotation = rotationAngle(dx, dy);
+            std::cout << "current goal: " << waypoints[waypoints_index].x << " " << waypoints[waypoints_index].y << std::endl;
 
-        ang_vel = ang_vel*bot_rotation;
+            double distance_to_point = dist(waypoints[waypoints_index], odo_.pose.pose.position);
 
-        std::cout << "ang_vel" << ang_vel << std::endl;
+            std::cout << "distance to point: " << distance_to_point << std::endl;
 
-        while (distance_to_point > linear_tolerance_){
-            double dx = waypoints_[waypoint_index].x - odo_.pose.pose.position.x;
-            double dy = waypoints_[waypoint_index].y - odo_.pose.pose.position.y;
+            double bot_rotation = rotationAngle(waypoints[waypoints_index], odo_.pose.pose.position);
 
-            distance_to_point = std::hypot(dx, dy);
+            while (distance_to_point > linear_tolerance_){
+            
+                distance_to_point = dist(waypoints[waypoints_index], odo_.pose.pose.position);
 
-            double bot_rotation = rotationAngle(dx, dy);
-            std::cout << "bot_rotation before loop: " << bot_rotation << std::endl;
+                bot_rotation = rotationAngle(waypoints[waypoints_index], odo_.pose.pose.position);
 
-            while(std::fabs(bot_rotation) > angular_tolerance_){
+                std::cout << "bot_rotation before loop: " << bot_rotation << std::endl;
+
+                while(std::fabs(bot_rotation) > angular_tolerance_){
+                    
+                    ROS_INFO("Turtlebot is not facing the target yet...");
+                   
+                    bot_rotation = rotationAngle(waypoints[waypoints_index], odo_.pose.pose.position);
+                    ang_vel_ = 0.3*bot_rotation;
+
+                    sendCommand(0, ang_vel_);
+                    ros::spinOnce();
+                    rate_.sleep();
+                }
+
+                ROS_INFO("Turtlebot is facing the target!");
+                lin_vel_ = 0.3;
+                ang_vel_ = 0;
+            
+                ROS_INFO_STREAM("Linear FT Velocity: " << lin_vel_);
+                ROS_INFO_STREAM("Angular FT Velocity: " << ang_vel_);
+
+                sendCommand(lin_vel_, ang_vel_);
                 
-                ROS_INFO("Turtlebot is not facing the target yet...");
-                ROS_INFO_STREAM("Linear NFT Velocity: " << lin_vel);
-                ROS_INFO_STREAM("Angular NFT Velocity: " << ang_vel);
-                double dx = waypoints_[waypoint_index].x - odo_.pose.pose.position.x;
-                double dy = waypoints_[waypoint_index].y - odo_.pose.pose.position.y;
-                bot_rotation = rotationAngle(dx, dy);
-                ang_vel = 0.4*bot_rotation;
-                std::cout << "bot_rotation in rotating loop: " << bot_rotation << std::endl;
-                sendCommand(0, ang_vel);
                 ros::spinOnce();
                 rate_.sleep();
-                std::cout << "bot_rotation in rotating loop after spinOnce: " << bot_rotation << std::endl;
-                std::cout << "angular tolerance: " << angular_tolerance_ << std::endl;
-                std::cout << "distance to point: " << distance_to_point << std::endl;
+
+                distance_to_point = dist(waypoints[waypoints_index], odo_.pose.pose.position);
+
+                std::cout << "distance to point: " << distance_to_point << std::endl;            
 
             }
-            std::cout << "distance to point: " << distance_to_point << std::endl;
 
-            ROS_INFO("Turtlebot is facing the target!");
-            lin_vel = 0.1;
-            ang_vel = 0;
-        
-            ROS_INFO_STREAM("Linear FT Velocity: " << lin_vel);
-            ROS_INFO_STREAM("Angular FT Velocity: " << ang_vel);
+            //within next waypoint tolerance
 
-            sendCommand(lin_vel, ang_vel);
-            
-            ros::spinOnce();
-            rate_.sleep();
+            double velocity = getVelocity(odo_);
 
-            distance_to_point = std::hypot(dx, dy);
+            // if approaching last waypoint (Goal) in set
+            if (waypoints_index == waypoints.size()-1){
+                std::cout << "reached final waypoint in set" << std::endl;
+                while (velocity > 0.01) {
+                    stopTurtlebot();
+                    ROS_INFO("STOPPING TURTLEBOT");
+                    velocity = getVelocity(odo_);
+                    ROS_INFO("linear velocity: %f", velocity);
+                    ros::spinOnce();
+                    rate_.sleep();
+                }
 
-            std::cout << "distance to point: " << distance_to_point << std::endl;
-            std::cout << "linear tolerance: " << linear_tolerance_ << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+            }
 
-        
+            while (velocity > 0.35) {
+                stopTurtlebot();
+                ROS_INFO("max velocity reached, slowing turtlebot");
+                std::cout << "linear velocity: " << velocity << std::endl; 
+                velocity = getVelocity(odo_);
+                ros::spinOnce();
+                rate_.sleep();
+            }
 
-        }
-
-        double velocity = getVelocity(odo_);
-        
-        while (velocity > 0.11) {
             stopTurtlebot();
-            ROS_INFO("STOPPING TURTLEBOT");
-            std::cout << "linear velocity: " << velocity << std::endl; 
-            velocity = getVelocity(odo_);
+            ROS_INFO("Reached waypoint!");
+            waypoints_index++;
         }
 
-        stopTurtlebot();
-        ROS_INFO("Reached waypoint!!!!!!!!!!!!!!!!!!!!!!!");
-        waypoint_index++;
+        //next set of waypoints
 
-        
-
-        // PID controller for steering
-
-        // if (odo_.twist.twist.linear.x > 0.5 || odo_.twist.twist.linear.x < 0.1){
-        //     lin_vel = 0.3;
-        // }
-
-        // if (std::abs(bot_rotation) < 0.1){
-        //     ang_vel = 0;
-
-        //     lin_vel = 0.3;
-        //     ROS_INFO("Turtlebot is facing the target");
-        // }
-
-        
-
-        // // Change the linear velocity halfway through the waypoints
-        // // if (waypoint_index > waypoints_.size()/2){
-        // //     lin_vel = 0.1; 
-        // // }
-
-        // if (distance_to_point < 0.5){
-
-        //     ROS_INFO_STREAM("Reached waypoint: " << waypoint_index);
-
-        //     sendCommand(0, 0);
-
-        //     waypoint_index++;
-            
-        //     // if (waypoint_index >= waypoints_.size()){
-        //     //     ROS_INFO("Reached goal");
-        //     //     sendCommand(0, 0);
-        //     //     return;
-        //     // }
-
-        // }
-
-        // std::cout <<"command about to be sent" << std::endl;
-        // ROS_INFO_STREAM("Linear NFT Velocity: " << lin_vel); 
-        // ROS_INFO_STREAM("Angular NFT Velocity: " << ang_vel);
-        // ROS_INFO_STREAM("Distance to point: " << distance_to_point);
-        // ROS_INFO_STREAM("Odom position: " << odo_.pose.pose.position.x << ", " << odo_.pose.pose.position.y);
-
-
-        // sendCommand(lin_vel, ang_vel);
-
-        // ros::spinOnce();
-        // rate_.sleep();
+        waypointSet_index++;
+ 
     }
     std::cout << "reached final goal" << std::endl;
 
@@ -372,26 +207,23 @@ void TurtlebotControl::odoCallback(const nav_msgs::Odometry::ConstPtr& msg){
 
     ROS_INFO("Position: x = %f, y = %f", odo_.pose.pose.position.x, odo_.pose.pose.position.y);
 
-    std::cout << "odo:" << odo_.pose.pose.position.x << " " << odo_.pose.pose.position.y << std::endl;
     odom_received_ = true;
 }
 
-double TurtlebotControl::rotationAngle(double x, double y){
+double TurtlebotControl::rotationAngle(geometry_msgs::Point p1 , geometry_msgs::Point p2){
 
-    double odoYaw;
     
     // Calculate the angle of rotation
-    double angle = std::atan2(y, x);
+    double dx = p1.x - p2.x;
+    double dy = p1.y - p2.y;
+
+    double angle = std::atan2(dy, dx);
 
     // Get the yaw of the odometry
-    odoYaw = tf::getYaw(odo_.pose.pose.orientation);
-
-    std::cout << "odoYaw: " << odoYaw << std::endl;
+    double odoYaw = tf::getYaw(odo_.pose.pose.orientation);
 
     // Calculate the difference in angles
     double angleDiff = angle - odoYaw;
-
-    std::cout << "angleDiff: " << angleDiff << std::endl;
 
     while (angleDiff > M_PI) {
         angleDiff -= 2*M_PI;
@@ -423,4 +255,8 @@ double TurtlebotControl::getVelocity(nav_msgs::Odometry odo){
 
     return velocity;
     
+}
+
+double TurtlebotControl::dist(geometry_msgs::Point p1, geometry_msgs::Point p2){
+    return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
 }
--- turtlebot_project/src/turtlebot_control.h
+++ turtlebot_project/src/turtlebot_control.h
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cmath>
-#include <vector> 
+#include <vector>
+#include <fstream> 
+#include <thread>
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
 #include <nav_msgs/Odometry.h>
+
 
 
 class TurtlebotControl{
@@ -17,8 +20,7 @@ class TurtlebotControl{
     // Initiates moving the turtlebot to goal.
     void moveTurtlebot();
 
-    void setWaypoints(std::vector<geometry_msgs::Point> waypoints);
-    
+    void setWaypoints(std::vector<std::vector<geometry_msgs::Point>> waypoints);
 
     void sendCommand(double throttle, double steering);
 
@@ -26,11 +28,15 @@ class TurtlebotControl{
 
     void odoCallback(const nav_msgs::Odometry::ConstPtr& msg);
 
-    double rotationAngle(double x, double y);
+    double rotationAngle(geometry_msgs::Point p1 , geometry_msgs::Point p2);
 
     void stopTurtlebot();
 
     double getVelocity(nav_msgs::Odometry odo);
+
+    void loadWaypoints_(std::string& file_path);
+
+    double dist(geometry_msgs::Point p1, geometry_msgs::Point p2);
 
     ros::NodeHandle nh_; // Node handle for communication with ROS
     int turtlebot_ID_; // ID of the turtlebot
@@ -43,13 +49,18 @@ class TurtlebotControl{
 
     nav_msgs::Odometry odo_; // Odometry information
 
-    std::vector<geometry_msgs::Point> waypoints_; // Waypoints to reach goal
-    geometry_msgs::Point currentWayPoint_; // Current waypoint to reach
+    std::vector<std::vector<geometry_msgs::Point>> waypointSet_; // Set of Waypoints to reach all goals
+    std::vector<geometry_msgs::Point> currentWaypoints_; // Waypoints to reach goal
+    geometry_msgs::Point currentPoint_; // Current waypoint to reach
     geometry_msgs::Point currentGoal_; // Goal to reach 
+    unsigned int current_waypoint_index_;
     unsigned int current_point_index_;
     double linear_tolerance_;
     double angular_tolerance_;
     bool odom_received_;
+
+    double lin_vel_;
+    double ang_vel_;
 
 
 
